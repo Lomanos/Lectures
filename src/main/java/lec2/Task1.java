a0884026667ca5bee57c053af191c2886f0efd1c
zlob <zlob.jc@gmail.com>
2021-05-18 01:40:33 +1000

    improve Task1

--- src/main/java/lec2/Task1.java
+++ src/main/java/lec2/Task1.java
@@ -1,16 +1,10 @@
 package lec2;
 
-public class task1 {
-
-    public static void main(String[] args) {
-        //npe();
-        //aioobe();
-        myThrow();
-    }
+public class Task1 {
 
     public static void npe() {
         String str = null;
-        System.out.println(""+str.length());
+        System.out.println("" + str.length());
     }
 
     public static void aioobe() {
@@ -20,10 +14,11 @@ public class task1 {
 
     public static void myThrow() {
         try {
-            System.out.println(1/0);
-        }catch (ArithmeticException q){
+            System.out.println(1 / 0);
+        } catch (ArithmeticException q) {
             throw new RuntimeException("something goes wrong", q);
         }
 
     }
+
 }
